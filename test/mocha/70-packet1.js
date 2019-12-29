9fdc3f398abc74b02bb36e0735d7753a99128048
Matthew Collier <mcollier@digitalbazaar.com>
2019-12-29 16:16:21 -0500

    Set chunk size to 80.

--- test/mocha/70-packet1.js
+++ test/mocha/70-packet1.js
@@ -25,7 +25,7 @@ describe('node-catchup', () => {
 
     const results = [];
     const maxDepth = 150;
-    for(let chunkSize = 10; chunkSize <= 300; chunkSize += 5) {
+    for(let chunkSize = 10; chunkSize <= 80; chunkSize += 5) {
       let totalEvents = 0;
       let totalOperations = 0;
       const masterTimer = _util.getTimer();
